abd0c5049003645b2099f60d4df17633491d0e98
leopham <hongthaipro@gmail.com>
2025-06-04 11:48:57 +0700

    feat: Initialize hello-solana client and shared libraries
    - Added autogenerated index.ts for hello-solana client.
    - Introduced shared library with utility functions for account validation and transaction signing.
    - Configured package.json for clients with dependencies on @solana/kit and TypeScript.
    - Set up TypeScript configuration for clients to support ESNext features and bundler mode.
    
    chore: Set up test-on-devnet environment
    
    - Created test-on-devnet project with basic structure and dependencies.
    - Implemented a simple index.ts to log a message.
    - Added shared library for API interactions with Solana.
    - Developed a test for creating a token using the hello-solana client.
    - Configured TypeScript settings for test-on-devnet to ensure compatibility with ESNext.
    
    build: Add .gitignore files and bun.lock for dependency management
    
    - Included .gitignore files to exclude unnecessary files from version control.
    - Generated bun.lock file to lock dependencies for the test-on-devnet project.

---
+++ basics/hello-solana/tests/hello_solana.test.ts
@@ -0,0 +1,35 @@
+import { expect, test } from 'bun:test';
+import { getApi } from '@test/shared/lib';
+import {
+  addSignersToTransactionMessage,
+  appendTransactionMessageInstruction,
+  createTransactionMessage,
+  pipe,
+  setTransactionMessageFeePayer,
+  setTransactionMessageLifetimeUsingBlockhash,
+  signTransactionMessageWithSigners,
+} from '@solana/kit';
+import { getHelloInstruction } from '@clients/hello-solana';
+
+test('tokens:create-token', async () => {
+  const { defaultPayer, rpc, sendAndConfirmTransaction } = await getApi();
+  let { value: latestBlockhash } = await rpc.getLatestBlockhash().send();
+  console.log(defaultPayer.address);
+
+  const transactionMintNftMessage = pipe(
+    createTransactionMessage({
+      version: 0,
+    }),
+    (tx) => setTransactionMessageFeePayer(defaultPayer.address, tx),
+    (tx) => setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, tx),
+    (tx) => appendTransactionMessageInstruction(getHelloInstruction(), tx),
+    (tx) => addSignersToTransactionMessage([defaultPayer], tx)
+  );
+  const signedTransactionMintNft = await signTransactionMessageWithSigners(
+    transactionMintNftMessage
+  );
+
+  await sendAndConfirmTransaction(signedTransactionMintNft, {
+    commitment: 'confirmed',
+  });
+});
